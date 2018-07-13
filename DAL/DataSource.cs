b1e6699a20d0d69335a46fa8b8cfe1feaa387447
kostyaHrytsyuk <streetman889@gmail.com>
2018-07-13 21:18:22 +0300

    Commented repositories for Pilot and Steward entities. Tested app for them

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -15,9 +15,9 @@ namespace DAL
 
         public List<Crew> Crews { get; set; }
 
-        public List<Pilot> Pilots { get; set; }
+        private readonly List<Pilot> pilots;
 
-        public List<Stewardess> Stewardesses { get; set; }
+        private readonly List<Stewardess> stewardesses;
 
         private readonly List<Plane> planes;
 
@@ -25,6 +25,7 @@ namespace DAL
 
         public DataSource()
         {
+            #region Plane Types
             planeTypes = new List<PlaneType>()
             {
                 new PlaneType() { Id = 1, PlaneModel = "Civil", SeatsNumber = 200, Carrying = 10000 },
@@ -32,24 +33,49 @@ namespace DAL
                 new PlaneType() { Id = 3, PlaneModel = "Military", SeatsNumber = 40, Carrying = 3000},
                 new PlaneType() { Id = 4, PlaneModel = "Falcon", SeatsNumber = 100, Carrying = 9}
             };
+            #endregion
 
+            #region Planes
             planes = new List<Plane>()
             {
                 new Plane(1, "Dream", 2, new DateTime(1995, 11, 4)),
                 new Plane(2, "Boeing", 1, new DateTime(2011, 6, 15)),
                 new Plane(3, "Heavy", 4, new DateTime(2018, 2, 6))
             };
+            #endregion
+
+            #region Stewardesses
+            stewardesses = new List<Stewardess>()
+            {
+                new Stewardess() { Id = 1, Name = "Katy", Surname = "Smith" , DateOfBirth = new DateTime(1983, 10, 5)},
+                new Stewardess() { Id = 2, Name = "Carol", Surname = "Hoover", DateOfBirth = new DateTime(1991, 4, 11)},
+                new Stewardess() { Id = 3, Name = "Ada", Surname = "Lovelace", DateOfBirth = new DateTime(1976, 7, 13)},
+                new Stewardess() { Id = 4, Name = "Beverly", Surname = "Marsh", DateOfBirth = new DateTime(2002 , 2, 13)}
+            };
+            #endregion
+
+            #region Pilots
+            pilots = new List<Pilot>()
+            {
+                new Pilot(){ Id = 1, Name = "Jimmy", Surname = "Doolittle", DateOfBirth = new DateTime(1973, 9, 16), Experience = 12},
+                new Pilot(){ Id = 2, Name = "Noel", Surname = "Wien", DateOfBirth = new DateTime(1982, 8 , 20), Experience = 9},
+                new Pilot(){ Id = 3, Name = "Robert", Surname = "Hoover", DateOfBirth = new DateTime(1980, 3 , 13), Experience = 15}
+            };
+            #endregion
+
 
             Data = new Dictionary<Type, IEnumerable<Entity>>();
             Data.Add(typeof(Departure), Departures);
             Data.Add(typeof(Flight), Flights);
             Data.Add(typeof(Ticket), Tickets);
             Data.Add(typeof(Crew), Crews);
-            Data.Add(typeof(Pilot), Pilots);
-            Data.Add(typeof(Stewardess), Stewardesses);
+            
 
             Data.Add(typeof(PlaneType), planeTypes);
             Data.Add(typeof(Plane), planes);
+            Data.Add(typeof(Stewardess), stewardesses);
+            Data.Add(typeof(Pilot), pilots);
+
 
         }
 
--- DAL/Repositories/StewardessRepository.cs
+++ DAL/Repositories/StewardessRepository.cs
@@ -1,45 +1,45 @@
-﻿using System.Collections.Generic;
-using DAL.Models;
+﻿//using System.Collections.Generic;
+//using DAL.Models;
 
-namespace DAL.Repositories
-{
-    class StewardessRepository : IRepository<Stewardess>
-    {
-        private DataSource _context;
+//namespace DAL.Repositories
+//{
+//    class StewardessRepository : IRepository<Stewardess>
+//    {
+//        private DataSource _context;
 
-        public StewardessRepository(DataSource context)
-        {
-            _context = context;
-        }
+//        public StewardessRepository(DataSource context)
+//        {
+//            _context = context;
+//        }
 
-        public List<Stewardess> GetAll()
-        {
-            return _context.Stewardesses;
-        }
+//        public List<Stewardess> GetAll()
+//        {
+//            return _context.Stewardesses;
+//        }
 
-        public Stewardess Get(int id)
-        {
-            return _context.Stewardesses.Find(s => s.Id == id);
-        }
+//        public Stewardess Get(int id)
+//        {
+//            return _context.Stewardesses.Find(s => s.Id == id);
+//        }
 
-        public void Create(Stewardess item)
-        {
-            _context.Stewardesses.Add(item);
-        }
+//        public void Create(Stewardess item)
+//        {
+//            _context.Stewardesses.Add(item);
+//        }
 
-        public void Update(Stewardess item)
-        {
-            var updItem = Get(item.Id);
-            updItem = item;
-        }
+//        public void Update(Stewardess item)
+//        {
+//            var updItem = Get(item.Id);
+//            updItem = item;
+//        }
 
-        public void Delete(int id)
-        {
-            var deleteItem = Get(id);
-            if (deleteItem != null)
-            {
-                _context.Stewardesses.Remove(deleteItem);
-            }
-        }
-    }
-}
+//        public void Delete(int id)
+//        {
+//            var deleteItem = Get(id);
+//            if (deleteItem != null)
+//            {
+//                _context.Stewardesses.Remove(deleteItem);
+//            }
+//        }
+//    }
+//}
