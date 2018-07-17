46a8ee96b371ad11150ac755713ab5940dc081ed
kostyaHrytsyuk <streetman889@gmail.com>
2018-07-17 03:51:16 +0300

    Code refactoring

--- DAL/Repositories/Repository.cs
+++ DAL/Repositories/Repository.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using DAL.Models;
-using System;
 
 namespace DAL.Repositories
 {
@@ -19,34 +18,89 @@ namespace DAL.Repositories
         {
             switch (typeof(T).Name)
             {
+                #region Plane
                 case "Plane":
+
                     var planes = _context.Planes;
+
                     var planesWithTypes = planes.Include(p => p.PlaneType);
+
                     return planesWithTypes.ToList() as List<T>;
+                #endregion
+
+                #region Stewardess
                 case "Stewardess":
+
                     var stewardesses = _context.Stewardesses;
-                    var stewardessesWithCrews = stewardesses.Include(s => s.Crew).ThenInclude(c => c.Pilot);
+
+                    var stewardessesWithCrews = stewardesses
+                                        .Include(s => s.Crew)
+                                            .ThenInclude(c => c.Pilot);
+
                     return stewardessesWithCrews.ToList() as List<T>;
+                #endregion
+
+                #region Pilot
                 case "Pilot":
+
                     var pilots = _context.Pilots;
-                    var pilotsWithCrews = pilots.Include(p => p.Crew).ThenInclude(c => c.Stewardesses);
+
+                    var pilotsWithCrews = pilots
+                                        .Include(p => p.Crew)
+                                            .ThenInclude(c => c.Stewardesses);
+
                     return pilotsWithCrews.ToList() as List<T>;
+                #endregion
+
+                #region Crew
                 case "Crew":
+
                     var crews = _context.Crews;
-                    var crewsWithStaff = crews.Include(c => c.Stewardesses).Include(c => c.Pilot);
+
+                    var crewsWithStaff = crews
+                                .Include(c => c.Stewardesses)
+                                .Include(c => c.Pilot);
+
                     return crewsWithStaff.ToList() as List<T>;
+                #endregion
+
+                #region Ticket
                 case "Ticket":
+
                     var tickets = _context.Tickets;
+
                     var ticketsWithFlights = tickets.Include(t => t.Flight);
+
                     return ticketsWithFlights.ToList() as List<T>;
+                #endregion
+
+                #region Flight
                 case "Flight":
+
                     var flights = _context.Flights;
+
                     var flightsWithTickets = flights.Include(f => f.Tickets);
+
                     return flightsWithTickets.ToList() as List<T>;
+                #endregion
+
+                #region Departure
                 case "Departure":
+
                     var departures = _context.Departures;
-                    var departuresWithInfo = departures.Include(d => d.Crew).Include(d => d.Flight).Include(d => d.Plane);
+
+                    var departuresWithInfo = departures
+                                .Include(d => d.Crew)
+                                    .ThenInclude(c => c.Pilot)
+                                    .ThenInclude(c => c.Crew.Stewardesses)
+                                .Include(d => d.Flight)
+                                    .ThenInclude(f => f.Tickets)
+                                .Include(d => d.Plane)
+                                    .ThenInclude(p => p.PlaneType);
+
                     return departuresWithInfo.ToList() as List<T>;
+                #endregion
+
                 default:
                     return _context.SetOf<T>().ToList() as List<T>;
             }
