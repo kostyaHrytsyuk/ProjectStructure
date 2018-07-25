f3264fac008006dc9b037693c7fd77ea30cae751
kostyaHrytsyuk <streetman889@gmail.com>
2018-07-25 05:05:40 +0300

    Created tests for Plane Type and Stewardess entities services

--- Airport.Tests/UnitTests/PlaneTypeServiceTest.cs
+++ Airport.Tests/UnitTests/PlaneTypeServiceTest.cs
@@ -16,19 +16,19 @@ namespace Airport.Tests.UnitTests
         private IRepository<PlaneType> _repository;
         private IPlaneTypeService _service;
         private IUnitOfWork _unitOfWork;
-        private PlaneTypeDto testPlaneType;
+        private PlaneTypeDto testPlaneTypeDto;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Init()
         {
-            testPlaneType = new PlaneTypeDto()
+            testPlaneTypeDto = new PlaneTypeDto()
             {
                 Id = 1,
                 PlaneModel = "Plane",
                 SeatsNumber = 200,
                 Carrying = 1000
             };
-
+                        
             _mapper = new Mapper(
                 new MapperConfiguration(cfg =>
                 {
@@ -36,6 +36,7 @@ namespace Airport.Tests.UnitTests
                     cfg.CreateMap<PlaneTypeDto, PlaneType>();
                 })
              );
+
             _repository = A.Fake<IRepository<PlaneType>>();
             _unitOfWork = A.Fake<IUnitOfWork>();
             A.CallTo(() => _unitOfWork.Repository<PlaneType>()).Returns(_repository);
@@ -43,34 +44,53 @@ namespace Airport.Tests.UnitTests
         }
 
         [Test]
-        public void Create_PlaneType_Should_Create_Ticket_Then_Equal_Last_Ticket()
+        public void Create_PlaneType_Should_Invoke_Repository_Create_Once()
         {
-            _service.Create(testPlaneType);
+            _service.Create(testPlaneTypeDto);
 
-            A.CallTo(() => _unitOfWork.Save()).MustHaveHappened(Repeated.Exactly.Once);
+            A.CallTo(_repository).Where(call => call.Method.Name == "Create").MustHaveHappenedOnceExactly();
+            A.CallTo(() => _unitOfWork.Save()).MustHaveHappenedOnceExactly();
         }
-        
+
+        [Test]
+        public void Update_PlaneType_Should_Invoke_Repository_Update_Once()
+        {
+            _service.Update(testPlaneTypeDto);
+
+            A.CallTo(_repository).Where(call => call.Method.Name == "Update").MustHaveHappenedOnceExactly();
+            A.CallTo(() => _unitOfWork.Save()).MustHaveHappenedOnceExactly();
+        }
+
         [Test]
         public void Map_PlaneType_From_Dto_To_Model_Then_Entities_Are_Equal_()
         {
-            var planeTypeDto = _mapper.Map<PlaneTypeDto,PlaneType>(testPlaneType);
+            var planeType = _mapper.Map<PlaneTypeDto,PlaneType>(testPlaneTypeDto);
+
+            Assert.AreEqual(planeType.Id, testPlaneTypeDto.Id);
+            Assert.AreEqual(planeType.PlaneModel, testPlaneTypeDto.PlaneModel);
+            Assert.AreEqual(planeType.SeatsNumber, testPlaneTypeDto.SeatsNumber);
+            Assert.AreEqual(planeType.Carrying, testPlaneTypeDto.Carrying);
+        }
+
+        [Test]
+        public void Map_PlaneType_From_Model_To_Dto_Then_Entities_Are_Equal_()
+        {
+            var testPlaneType = new PlaneType()
+            {
+                Id = 2,
+                PlaneModel = "Avia",
+                SeatsNumber = 500,
+                Carrying = 2000
+            };
+
+
+            var planeTypeDto = _mapper.Map<PlaneType, PlaneTypeDto>(testPlaneType);
 
             Assert.AreEqual(planeTypeDto.Id, testPlaneType.Id);
             Assert.AreEqual(planeTypeDto.PlaneModel, testPlaneType.PlaneModel);
             Assert.AreEqual(planeTypeDto.SeatsNumber, testPlaneType.SeatsNumber);
             Assert.AreEqual(planeTypeDto.Carrying, testPlaneType.Carrying);
         }
-
-        //[Test]
-        //public void Map_PlaneType_From_Model_To_Dto_Then_Entities_Are_Equal_()
-        //{
-        //    var planeTypeModel = _mapper.Map<PlaneType, PlaneTypeDto>(testPlaneType);
-
-        //    Assert.AreEqual(planeTypeModel.Id, testPlaneType.Id);
-        //    Assert.AreEqual(planeTypeModel.PlaneModel, testPlaneType.PlaneModel);
-        //    Assert.AreEqual(planeTypeModel.SeatsNumber, testPlaneType.SeatsNumber);
-        //    Assert.AreEqual(planeTypeModel.Carrying, testPlaneType.Carrying);
-        //}
 
 
 
--- Airport.Tests/UnitTests/StewardessServiceTest.cs
+++ Airport.Tests/UnitTests/StewardessServiceTest.cs
@@ -6,6 +6,7 @@ using DAL.Repositories;
 using DAL.UnitOfWork;
 using FakeItEasy;
 using NUnit.Framework;
+using System;
 
 
 namespace Airport.Tests.UnitTests
@@ -13,39 +14,90 @@ namespace Airport.Tests.UnitTests
     [TestFixture]
     public class StewardessServiceTest
     {
-
-        private Mapper _mapper;
+        private IMapper _mapper;
         private IRepository<Stewardess> _repository;
         private IStewardessService _service;
         private IUnitOfWork _unitOfWork;
+        private StewardessDto testStewardessDto;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Init()
         {
+            testStewardessDto = new StewardessDto()
+            {
+                Id = 1,
+                FirstName = "First",
+                LastName = "Last",
+                BirthDate = DateTime.Now,
+                CrewId = 1
+            };
+
             _mapper = new Mapper(
                 new MapperConfiguration(cfg =>
                 {
-                   cfg.CreateMap<Stewardess, StewardessDto>();
-                   cfg.CreateMap<StewardessDto, Stewardess>();
+                    cfg.CreateMap<Stewardess, StewardessDto>();
+                    cfg.CreateMap<StewardessDto, Stewardess>();
                 })
-             ); 
+             );
+
             _repository = A.Fake<IRepository<Stewardess>>();
             _unitOfWork = A.Fake<IUnitOfWork>();
             A.CallTo(() => _unitOfWork.Repository<Stewardess>()).Returns(_repository);
             _service = new StewardessService(_unitOfWork, _mapper);
-
-            
         }
 
         [Test]
-        public void Get_Stewardess_With_Fake_Id_Then_Id_Equal_Zero()
+        public void Create_Stewardess_Should_Invoke_Repository_Create_Once()
         {
-            var fakeId = -1;
+            _service.Create(testStewardessDto);
 
-            var stewardess = _service.Get(fakeId);
-            
-            Assert.AreEqual(stewardess.Id,0);
+            A.CallTo(_repository).Where(call => call.Method.Name == "Create").MustHaveHappenedOnceExactly();
+            A.CallTo(() => _unitOfWork.Save()).MustHaveHappenedOnceExactly();
 
         }
+
+        [Test]
+        public void Update_Stewardess_Should_Invoke_Repository_Update_Once()
+        {
+            _service.Update(testStewardessDto);
+
+            A.CallTo(_repository).Where(call => call.Method.Name == "Update").MustHaveHappenedOnceExactly();
+            A.CallTo(() => _unitOfWork.Save()).MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void Map_Stewardess_From_Dto_To_Model_Then_Entities_Are_Equal_()
+        {
+            var stewardess = _mapper.Map<StewardessDto, Stewardess>(testStewardessDto);
+
+            Assert.AreEqual(stewardess.Id, testStewardessDto.Id);
+            Assert.AreEqual(stewardess.FirstName, testStewardessDto.FirstName);
+            Assert.AreEqual(stewardess.LastName, testStewardessDto.LastName);
+            Assert.AreEqual(stewardess.BirthDate, testStewardessDto.BirthDate);
+            Assert.AreEqual(stewardess.CrewId, testStewardessDto.CrewId);
+        }
+
+        [Test]
+        public void Map_Stewardess_From_Model_To_Dto_Then_Entities_Are_Equal_()
+        {
+            var testStewardess = new Stewardess()
+            {
+                Id = 1,
+                FirstName = "Tesname",
+                LastName = "Testlast",
+                BirthDate = new DateTime(2003, 8, 15),
+                CrewId = 2
+            };
+
+            var stewardessDto = _mapper.Map<Stewardess, StewardessDto>(testStewardess);
+
+            Assert.AreEqual(stewardessDto.Id, testStewardess.Id);
+            Assert.AreEqual(stewardessDto.FirstName, testStewardess.FirstName);
+            Assert.AreEqual(stewardessDto.LastName, testStewardess.LastName);
+            Assert.AreEqual(stewardessDto.BirthDate, testStewardess.BirthDate);
+            Assert.AreEqual(stewardessDto.CrewId, testStewardess.CrewId);
+
+        }
+
     }
 }
