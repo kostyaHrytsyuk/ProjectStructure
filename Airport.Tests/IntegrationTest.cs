e5258ecdfdad541d51f6cfdf44eb0f5c5bd34f3b
kostyaHrytsyuk <streetman889@gmail.com>
2018-07-30 23:03:24 +0300

    Added integration tests for Flight and Crew entites

--- Airport.Tests/IntegrationTest.cs
+++ Airport.Tests/IntegrationTest.cs
@@ -6,6 +6,7 @@ using DAL.Models;
 using DAL.UnitOfWork;
 using NUnit.Framework;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 
@@ -23,20 +24,40 @@ namespace Airport.Tests
         {
             _context = TestSettings.Context;
             _unitOfWork = new UnitOfWork(_context);
-            
+            _mapper = new Mapper(
+                new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<PlaneType, PlaneTypeDto>();
+                    cfg.CreateMap<PlaneTypeDto, PlaneType>();
+
+                    cfg.CreateMap<Plane, PlaneDto>();
+                    cfg.CreateMap<PlaneDto, Plane>();
+
+                    cfg.CreateMap<Stewardess, StewardessDto>();
+                    cfg.CreateMap<StewardessDto, Stewardess>();
+
+                    cfg.CreateMap<Pilot, PilotDto>();
+                    cfg.CreateMap<PilotDto, Pilot>();
+
+                    cfg.CreateMap<Crew, CrewDto>();
+                    cfg.CreateMap<CrewDto, Crew>();
+
+                    cfg.CreateMap<Ticket, TicketDto>();
+                    cfg.CreateMap<TicketDto, Ticket>();
+
+                    cfg.CreateMap<Flight, FlightDto>();
+                    cfg.CreateMap<FlightDto, Flight>();
+
+                    cfg.CreateMap<Departure, DepartureDto>();
+                    cfg.CreateMap<DepartureDto, Departure>();
+                })
+            );
         }
 
         [Test]
         public void Get_All_Stewardesses_And_Get_By_Id_With_The_Same_Id_Are_Equal()
         {
             //Arrange
-            _mapper = new Mapper(
-                 new MapperConfiguration(cfg =>
-                 {
-                     cfg.CreateMap<Stewardess, StewardessDto>();
-                     cfg.CreateMap<StewardessDto, Stewardess>();
-                 })
-              );
             var stewardessService = new StewardessService(_unitOfWork, _mapper);
 
             //Act
@@ -53,6 +74,91 @@ namespace Airport.Tests
         }
 
         [Test]
+        public void Create_Crew_And_Stewardesses_With_Same_Crew_Id_Then_Crew_Should_Contain_These_Stewardesses()
+        {
+            //Arrange
+            var testCrewDto = new CrewDto()
+            {
+                PilotId = 1
+            };
+            var stewardessService = new StewardessService(_unitOfWork, _mapper);
+            var crewService = new CrewService(_unitOfWork, _mapper);
+
+            //Act
+            testCrewDto = crewService.Create(testCrewDto);
+
+            var testStewardessOne = new StewardessDto()
+            {
+                FirstName = "First",
+                LastName = "Last",
+                BirthDate = new DateTime(1974, 5, 8),
+                CrewId = testCrewDto.Id
+            };
+            var testStewardessSecond = new StewardessDto()
+            {
+                FirstName = "Firstone",
+                LastName = "Lastone",
+                BirthDate = new DateTime(1984, 5, 8),
+                CrewId = testCrewDto.Id
+            };
+
+            testStewardessOne = stewardessService.Create(testStewardessOne);
+            testStewardessSecond = stewardessService.Create(testStewardessSecond);
+
+            var crewWithStewardesses = crewService.Get(testCrewDto.Id);
+
+            //Assert
+            Assert.True(crewWithStewardesses.Stewardesses.Any());
+            Assert.AreEqual(2,crewWithStewardesses.Stewardesses.Count);
+            Assert.AreEqual(crewWithStewardesses.Stewardesses.FirstOrDefault().Id, testStewardessOne.Id);
+            Assert.AreEqual(crewWithStewardesses.Stewardesses.LastOrDefault().Id,  testStewardessSecond.Id);
+        }
+
+        [Test]
+        public void Create_Flight_And_Tickets_With_Same_Flight_Id_Then_Flight_Should_Contain_These_Tickets()
+        {
+            //Arrange
+            var testFlightDto = new FlightDto()
+            {
+                FlightNumber = "FK7684",
+                DepartureAirport = "DGK",
+                DestinationAirport = "FIR"
+            };
+
+            var ticketService = new TicketService(_unitOfWork, _mapper);
+            var flightService = new FlightService(_unitOfWork, _mapper);
+
+            //Act
+            testFlightDto = flightService.Create(testFlightDto);
+
+            var testTicketOne = new TicketDto()
+            {
+                FlightId = testFlightDto.Id,
+                FlightNumber = testFlightDto.FlightNumber,
+                Price = 200
+            };
+            var testTicketSecond = new TicketDto()
+            {
+                FlightId = testFlightDto.Id,
+                FlightNumber = testFlightDto.FlightNumber,
+                Price = 250
+            };
+
+            testTicketOne = ticketService.Create(testTicketOne);
+            testTicketSecond = ticketService.Create(testTicketSecond);
+
+            var flightWithTickets = flightService.Get(testFlightDto.Id);
+
+            //Assert
+            Assert.True(flightWithTickets.Tickets.Any());
+            Assert.AreEqual(2, flightWithTickets.Tickets.Count());
+            Assert.AreEqual(flightWithTickets.Tickets.FirstOrDefault().Id, testTicketOne.Id);
+            Assert.AreEqual(flightWithTickets.Tickets.LastOrDefault().Id, testTicketSecond.Id);
+
+        }
+        
+
+        [Test]
         public void Create_Plane_Should_Return_Existing_Item()
         {
             //Arrange
@@ -62,13 +168,7 @@ namespace Airport.Tests
                 Name = "Jet",
                 ReleaseDate = new DateTime(2002, 2, 20)
             };
-            _mapper = new Mapper(
-                 new MapperConfiguration(cfg =>
-                 {
-                     cfg.CreateMap<Plane, PlaneDto>();
-                     cfg.CreateMap<PlaneDto, Plane>();
-                 })
-              );
+            
             var planeService = new PlaneService(_unitOfWork, _mapper);
 
             //Act
@@ -88,14 +188,7 @@ namespace Airport.Tests
                 SeatsNumber = 42,
                 Carrying = 30000
             };
-
-            _mapper = new Mapper(
-                 new MapperConfiguration(cfg =>
-                 {
-                     cfg.CreateMap<PlaneType, PlaneTypeDto>();
-                     cfg.CreateMap<PlaneTypeDto, PlaneType>();
-                 })
-              );
+                        
             var planeTypeService = new PlaneTypeService(_unitOfWork, _mapper);
 
             //Act
@@ -106,6 +199,7 @@ namespace Airport.Tests
             Assert.AreEqual(id, planeTypeService.Get(id).Id);
         }
 
+
         [Test]
         public void Update_Pilot_With_Not_Exist_Id_Then_Throws_ArgumentNullException()
         {
@@ -114,13 +208,7 @@ namespace Airport.Tests
             {
                 Id = -1
             };
-            _mapper = new Mapper(
-                 new MapperConfiguration(cfg =>
-                 {
-                     cfg.CreateMap<Pilot, PilotDto>();
-                     cfg.CreateMap<PilotDto, Pilot>();
-                 })
-              );
+            
             var pilotService = new PilotService(_unitOfWork, _mapper);
 
             //Act
@@ -134,16 +222,10 @@ namespace Airport.Tests
             //Arrange
             var testCrewDto = new CrewDto()
             {
-                Id = 1,
-                PilotId = 1
+                Id=2,
+                PilotId = 2
             };
-            _mapper = new Mapper(
-                 new MapperConfiguration(cfg =>
-                 {
-                     cfg.CreateMap<Crew, CrewDto>();
-                     cfg.CreateMap<CrewDto, Crew>();
-                 })
-              );
+            
             var crewService = new CrewService(_unitOfWork, _mapper);
 
             //Act
@@ -159,13 +241,7 @@ namespace Airport.Tests
         public void Delete_Ticket_With_Not_Exist_Id_Then_Throws_NullReferenceException()
         {
             //Arrange
-            _mapper = new Mapper(
-                 new MapperConfiguration(cfg =>
-                 {
-                     cfg.CreateMap<Ticket, TicketDto>();
-                     cfg.CreateMap<TicketDto, Ticket>();
-                 })
-              );
+            
             var ticketService = new TicketService(_unitOfWork, _mapper);
 
             //Act
@@ -177,13 +253,7 @@ namespace Airport.Tests
         public void Delete_Departure_Then_Deleted_Departure_Not_Exist()
         {
             //Arrange
-            _mapper = new Mapper(
-                 new MapperConfiguration(cfg =>
-                 {
-                     cfg.CreateMap<Departure, DepartureDto>();
-                     cfg.CreateMap<DepartureDto, Departure>();
-                 })
-              );
+            
             var departureService = new DepartureService(_unitOfWork, _mapper);
             var itemId = departureService.GetAll()[0].Id;
 
